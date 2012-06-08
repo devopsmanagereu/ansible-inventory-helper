7f924153228c99ee404b156a662e637ce4e542dc
Alexei <alexei.vasin@jetbrains.com>
2012-06-08 14:50:28 +0400

    Added intentions to remove duplicate sections and directives

--- src/ini4idea/IniAnnotator.java
+++ src/ini4idea/IniAnnotator.java
@@ -1,9 +1,19 @@
 package ini4idea;
 
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.containers.hash.HashSet;
+import ini4idea.lang.IniTokenTypes;
 import ini4idea.lang.psi.IniFile;
 import ini4idea.lang.psi.IniKeyImpl;
 import ini4idea.lang.psi.IniSectionImpl;
@@ -19,38 +29,45 @@ public class IniAnnotator implements Annotator {
 
 
     @Override
-    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+    public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         if (element instanceof IniFile) {
             Set<String> sectionSet = new HashSet<String>();
-
+            Annotation annotation;
             PsiElement children[] = element.getChildren();
 
             for (PsiElement elem : children) {
                 if (elem instanceof IniSectionImpl) {
                     PsiElement child = elem.getFirstChild();
-                    assert child != null;
-                    String sectionKey = child.getText();
+                    //assert child != null;
+                    if (child != null) {
+                        String sectionKey = child.getText();
 
-                    if (sectionSet.contains(sectionKey)) {
-                        holder.createWarningAnnotation(child, "Duplicate section");
-                    } else {
-                        sectionSet.add(sectionKey);
-                    }
+                        if (sectionSet.contains(sectionKey)) {
+                            annotation = holder.createWarningAnnotation(child, "Duplicate section");
+                            annotation.registerFix(new DuplicateSectionIntention(child));
+                        } else {
+                            sectionSet.add(sectionKey);
+                        }
 
-                    PsiElement sectionChildren[] = elem.getChildren();
-                    Set<String> s = new HashSet<String>();
+                        PsiElement sectionChildren[] = elem.getChildren();
+                        Set<String> s = new HashSet<String>();
 
-                    for (PsiElement sectionElement : sectionChildren) {
-                        if (sectionElement instanceof IniKeyImpl || sectionElement instanceof DirectiveImpl) {
-                            PsiElement e = sectionElement.getFirstChild();
+                        for (final PsiElement sectionElement : sectionChildren) {
+                            if (sectionElement instanceof IniKeyImpl || sectionElement instanceof DirectiveImpl) {
+                                PsiElement e = sectionElement.getFirstChild();
 
-                            assert e != null;
-                            String key = e.getText();
+                                assert e != null;
+                                String key = e.getText();
 
-                            if (s.contains(key)) {
-                                holder.createWarningAnnotation(e, "Duplicate key in the same section");
-                            } else {
-                                s.add(key);
+                                if (s.contains(key)) {
+                                    annotation = holder.createWarningAnnotation(e, "Duplicate key in the same section");
+
+                                    annotation.registerFix(new DuplicateDirectiveIntention(sectionElement));
+
+
+                                } else {
+                                    s.add(key);
+                                }
                             }
                         }
                     }
@@ -58,4 +75,64 @@ public class IniAnnotator implements Annotator {
             }
         }
     }
+
+    private static class DuplicateDirectiveIntention implements IntentionAction {
+
+        private PsiElement myElement;
+
+        private DuplicateDirectiveIntention(PsiElement myElement) {
+            this.myElement = myElement;
+        }
+
+        @NotNull
+        @Override
+        public String getText() {
+            return "Remove Duplicate Directive";
+        }
+
+        @NotNull
+        @Override
+        public String getFamilyName() {
+            return getText();
+        }
+
+        @Override
+        public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
+            return true;
+        }
+
+        @Override
+        public void invoke(@NotNull Project project, Editor editor, PsiFile file) throws IncorrectOperationException {
+            TextRange textRange;
+            if (myElement.getNode().getElementType() == IniTokenTypes.SECTION) {
+                textRange = myElement.getParent().getTextRange();
+            } else {
+                textRange = myElement.getTextRange();
+            }
+            if (textRange != null) {
+                Document document = PsiDocumentManager.getInstance(project).getDocument(myElement.getContainingFile());
+                document.deleteString(textRange.getStartOffset(), textRange.getEndOffset());
+            }
+        }
+
+        @Override
+        public boolean startInWriteAction() {
+            return true;
+        }
+    }
+
+    private static class DuplicateSectionIntention extends DuplicateDirectiveIntention {
+
+        private DuplicateSectionIntention(PsiElement myElement) {
+            super(myElement);
+        }
+
+        @NotNull
+        @Override
+        public String getText() {
+            return "Remove Duplicate Section";
+        }
+
+    }
+
 }
